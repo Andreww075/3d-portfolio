732897820ccb9e67bc5e6fda05dc836428cdf3f3
Andreww075 <151580368+Andreww075@users.noreply.github.com>
2024-09-23 18:46:18 -0500

    render 3d, render component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import Image from "next/image";
+import bg from "@/../public/background/home-background.png";
+import RenderModel from "@/components/render-model";
+import Wizard from "@/components/models/wizard";
+
 export default function Home() {
   return (
-    <div>MainPage</div>
+    <div className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image
+        priority
+        sizes="100vw"
+        src={bg}
+        alt="background-image"
+        fill
+        className="-z-50 w-full h-full object-cover object-center opacity-50"
+      />
+
+      <div className="w-full h-screen">
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
+      </div>
+    </div>
   );
 }
---
+++ src/components/render-model.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { Suspense } from "react";
+import { Environment } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
+
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+const RenderModel = ({ children, className }: Props) => {
+  return (
+    <Canvas
+      className={clsx("w-screen h-screen -z-10 relative", className)}
+      shadows={false}
+      dpr={[1, 2]} 
+    >
+      <Suspense fallback={null}>{children}</Suspense>
+      <Environment preset="dawn" />
+    </Canvas>
+  )
+};
+
+export default RenderModel;
