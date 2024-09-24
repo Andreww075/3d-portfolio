7f1200754fd961f1bc07b101f068992388f3a6c5
Andreww075 <151580368+Andreww075@users.noreply.github.com>
2024-09-24 09:43:43 -0500

    FireFliesBakcground add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import clsx from 'clsx'
+import clsx from 'clsx';
+import FireFliesBackground from "@/components/fire-flies-background";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -32,6 +33,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <FireFliesBackground />
       </body>
     </html>
   );
